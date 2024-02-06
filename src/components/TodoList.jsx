2a19cb34bd4eb0ed5a94a2f8160534075d4bad6c
KY <rudyeon@naver.com>
2024-02-06 15:46:11 +0900

    commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,53 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { switchTodo } from "../redux/modules/todos";
 
 function TodoList() {
+  const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos.todos);
+  const isDoneChangeBtn = () => {
+    dispatch(switchTodo());
+  };
+
   return (
     <div>
       <h3>TodoList</h3>
       <div>
-        {todos.map((todo) => (
-          <div key={todo.id}>
-            {todo.title} - {todo.content}
-          </div>
-        ))}
+        {todos
+          .filter((todo) => todo.isDone === false)
+          .map((todo) => (
+            <div
+              key={todo.id}
+              style={{ border: "1px solid green", width: "300px" }}
+            >
+              <p>
+                {todo.title} - {todo.content}
+              </p>
+              <button onClick={isDoneChangeBtn}>완료</button>
+              <button>삭제</button>
+              <button>상세보기</button>
+            </div>
+          ))}
+      </div>
+      <hr />
+      <h3>DoneList</h3>
+      <div>
+        {todos
+          .filter((todo) => todo.isDone === true)
+          .map((todo) => (
+            <div
+              key={todo.id}
+              style={{ border: "1px solid green", width: "300px" }}
+            >
+              <p>
+                {todo.title} - {todo.content}
+              </p>
+              <button>취소</button>
+              <button>삭제</button>
+              <button>상세보기</button>
+            </div>
+          ))}
       </div>
     </div>
   );
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -2,9 +2,15 @@
 import shortid from "shortid";
 
 const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const SWITCH_TODO = "SWITCH_TODO";
 
 export const addTodo = (payload) => {
   return { type: ADD_TODO, payload };
+};
+
+export const switchTodo = (payload) => {
+  return { type: SWITCH_TODO, payload };
 };
 
 const initialState = {
@@ -21,16 +27,16 @@ const initialState = {
 // 리듀서
 const todos = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return {
         ...state,
         todos: [...state.todos, action.payload],
       };
 
-    case "DELETE_TODO":
+    case DELETE_TODO:
       return; //TODO: 여기 작성
 
-    case "SWITCH_TODO":
+    case SWITCH_TODO:
       return; //TODO: 여기 작성
 
     default:
